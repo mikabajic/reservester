f7cc1c22babe63ff84cfeb21832940fb70606a0e
Milos Bajic <mikabajic@yahoo.com>
2014-07-18 23:47:26 -0500

    destroy

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -35,6 +35,13 @@ class RestaurantsController < ApplicationController
   	@restaurant = Restaurant.find(params[:id])
   end 
 
+  def destroy
+  	@restaurant = Restaurant.find(params[:id])
+  	@restaurant.destroy
+
+  	redirect_to restaurants_path 
+  end 
+
 	private
   def restaurant_params
     params.require(:restaurant).permit(:name, :description, :address, :phone)
