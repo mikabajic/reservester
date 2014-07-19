e0964ca5bd81d1b323d3c69267ef9ddba6dca9b3
Milos Bajic <mikabajic@yahoo.com>
2014-07-18 21:50:36 -0500

    error-restaurant name

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,24 @@
 class RestaurantsController < ApplicationController
 	def new
+		@restaurant = Restaurant.new
 	end
 	
 	def create
 		@restaurant = Restaurant.new(restaurant_params)
 
-		@restaurant.save
+		if @restaurant.save
 		redirect_to @restaurant
+	else
+		render 'new'
+	end
 	end 
 
 	def index
 		@restaurants = Restaurant.all
+	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
 	end
 
 
