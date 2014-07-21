e454e4a0144385cc0e994f7574c17891e17c1d66
Milos Bajic <mikabajic@yahoo.com>
2014-07-20 19:44:45 -0500

    model - uploader

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ActiveRecord::Base
 	validates :name, presence: true,
                     length: { minimum: 3 }
-                end 
 
 mount_uploader :image, ImageUploader 
 end 
 end 
+
