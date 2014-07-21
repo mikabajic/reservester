fb27149b3f6892c44a7faa85edf65f0fd6193cea
Milos Bajic <mikabajic@yahoo.com>
2014-07-21 12:04:11 -0500

    to_limit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,7 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   process :resize_to_fit => [800, 800]
 
   version :thumb do
-    process :resize_to_fill => [200,200]
+    process :resize_to_limit => [200,200]
   end
 
 
