5b7518f98e2d906a442dc810dd8206e853659231
Milos Bajic <mikabajic@yahoo.com>
2014-07-20 21:17:04 -0500

    uploaders-model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ActiveRecord::Base
 	validates :name, presence: true,
-                    length: { minimum: 3 }
+	length: { minimum: 3 }
+end
+
+	mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718180702) do
+ActiveRecord::Schema.define(version: 20140721021326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140718180702) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
