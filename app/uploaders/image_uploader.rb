9de85b1977cb7220437296091470bc4ca81483af
Milos Bajic <mikabajic@yahoo.com>
2014-07-22 11:35:13 -0500

    initializer

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  #include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
   process :resize_to_fit => [800, 800]
