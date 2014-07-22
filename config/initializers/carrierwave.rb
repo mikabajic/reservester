164c715d87f1eba2b7879756276f865c536caab2
Milos Bajic <mikabajic@yahoo.com>
2014-07-21 23:04:23 -0500

    remove Heroku line

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,6 +15,5 @@ CarrierWave.configure do |config|
   end
 
   config.cache_dir        = "#{Rails.root}/tmp/uploads" # To let 
-CarrierWave work on Heroku
   config.fog_directory    = ENV['S3_BUCKET_NAME']
 end
