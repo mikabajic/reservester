62364f4a00502e411501e72b36e294372b7ae73b
Milos Bajic <mikabajic@yahoo.com>
2014-07-22 19:54:56 -0500

    to_fill + recreate_versions

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,9 @@ class Restaurant < ActiveRecord::Base
 	validates :name, presence: true,
 	length: { minimum: 3 }
 	mount_uploader :image, ImageUploader
+	Restaurant.each do |restaurant|
+		restaurant.image.recreate_versions! if restaurant.image?
+	end
 end 
 
 
