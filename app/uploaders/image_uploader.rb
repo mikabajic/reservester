718f6f8f015487dfb41e015b872649b53bf2d0cb
Milos Bajic <mikabajic@yahoo.com>
2014-07-23 11:51:58 -0500

    thumb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   #include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  process :resize_to_fit => [800, 800]
 
   version :thumb do
     process :resize_to_fit => [200,200]
